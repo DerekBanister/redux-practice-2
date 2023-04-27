8f6bd8c3be706a6edcebe6ad232ab7b16dea4763
unknown <ptownderek@yahoo.com>
2023-04-27 10:04:46 -0700

    it didn't like calling dispatch in ac's, just returning type/payload fixed it

--- src/state/action-creators/index.js
+++ src/state/action-creators/index.js
@@ -3,20 +3,17 @@
 // this action creator gets called in the front-end
 // takes an amount as argument
 export const deposit = (amount) => {
-    return (dispatch) => {
-        dispatch({
-            type: 'deposit',
-            payload: amount
-        })
+    return {
+        type: 'deposit',
+        payload: amount
     }
 }
 
+
 export const withdraw = (amount) => {
-    return (dispatch) => {
-        dispatch({
-            type: 'withdraw',
-            payload: amount
-        })
+    return {
+        type: "withdraw",
+        payload: amount
     }
 }
 
